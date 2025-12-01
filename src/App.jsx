86acfee87b3ca1ee2ff652f790cc7c9ba92dbdad
Aiswarya <aiswaryab1810@gmail.com>
2025-12-01 16:21:14 +0530

    usercreation

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,11 @@ import './App.css'
 import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home/Home'
 import Navbar from './components/Navbar/Navbar'
-import { Test } from './pages/test'
+import UserCreation from './pages/UserCreation/UserCreation'
+import LedgerCreation from './pages/Ledgercreation/Ledgercreation'
+import LedgerGroupCreation from './pages/LedgerGroupCreation/Ledgergroupcreation'
+import ItemCreation from './pages/ItemCreation/ItemCreation'
+import ItemGroupCreation from './pages/ItemGroupCreation/ItemGroupCreation'
 function App() {
   return (
     <div className="app">
@@ -10,7 +14,11 @@ function App() {
       <main className="main-content">
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="test" element={<Test />} />
+          <Route path="/masters/ledger-creation" element={<LedgerCreation />} />
+          <Route path="/masters/ledger-group-creation" element={<LedgerGroupCreation />} />
+          <Route path="/masters/item-creation" element={<ItemCreation />} />
+          <Route path="/masters/item-group-creation" element={<ItemGroupCreation />} />
+          <Route path="/masters/User-creation" element={<UserCreation />} />
         </Routes>
       </main>
     </div>
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -78,6 +78,7 @@ const Navbar = () => {
     { name: 'Item Creation', path: '/masters/item-creation', icon: <BuildOutlined /> },
     { name: 'Unit Creation', path: '/masters/unit-creation', icon: <TeamOutlined /> },
     { name: 'Salesman Creation', path: '/masters/salesman-creation', icon: <UserOutlined /> },
+    { name: 'User Creation', path: '/masters/User-creation', icon: <BuildOutlined /> },
     { name: 'Company Creation', path: '/masters/company-creation', icon: <BuildOutlined /> }
   ];
 
