ad1dc65272a351371268a3877b2f38a9ebd713d9
Priyankasathyanarayanan <sathyanarayananpriyanka@gmail.com>
2025-12-10 12:24:25 +0530

    billcollector

--- src/api/endpoints.js
+++ src/api/endpoints.js
@@ -101,35 +101,9 @@ user_creation: {
     GET_SCRAP_BY_CODE: (scrapCode) => `/ScrapCreation/getScrapByCode/${scrapCode}`,
   },
 
-// ITEM_CREATION: {
-//     POST: `ItemCreation/ItemCreationPost`,
-//     PUT: `ItemCreation/ItemCreationUpdate`,
-//     DELETE: `ItemCreation/ItemCreationDelete`,
-//     GET_DROPDOWNS: `ItemCreation/GetItemCreationdropdowslist`,
-//     GET_MAX_PREFIX: `ItemCreation/GetMaxPrefix`,
-//     GET_ALL_ITEMS: `ItemCreation/GetAllItems`,
-//     GET_TREE_DATA: `ItemCreation/GetTreeData`,
-//   }
-  
-//   // Other endpoints
-//   BRANDS: `${BASE_URL}Brand/GetAllBrands`,
-//   CATEGORIES: `${BASE_URL}Category/GetAllCategories`,
-//   PRODUCTS: `${BASE_URL}Product/GetAllProducts`,
-//   MODELS: `${BASE_URL}Model/GetAllModels`,
-//   SIZES: `${BASE_URL}Size/GetAllSizes`,
-//   UNITS: `${BASE_URL}Unit/GetAllUnits`,
-// }
- 
 
 
-
-
-
-
-
-
-
- scrapratefix:
+  scrapratefix:
  {
   getscrapratefixing:"ScrapRateFixing/getFullScrapRateFixing",
   putscrapratefixing:"ScrapRateFixing/updateFullScrapRateFixing",
@@ -180,12 +154,7 @@ CATEGORY: {
   DELETE_CATEGORY: (code) => `CATEGORY/DeleteCategory/${code}`,
   GET_NEXT_CODE: 'CATEGORY/getNextModelFcode'
 }
-
-
-
-
-
-
 };
 
 export const ADMINISTRATION = API_ENDPOINTS.ADMINISTRATION;
+      
