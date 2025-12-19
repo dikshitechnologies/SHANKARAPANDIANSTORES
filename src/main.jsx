c1618295a169fc4045e57d4eabe9d5bafae2f0fb
md0927 <manisekar0927@gmail.com>
2025-12-19 15:35:00 +0530

    for build

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import { AuthProvider } from './context/AuthContext'
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
-      <BrowserRouter>
+      <BrowserRouter basename="/SPSTORE">
         <AuthProvider>
           <App />
         </AuthProvider>
