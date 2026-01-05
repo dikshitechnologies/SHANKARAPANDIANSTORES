67cdc34293453701929f6bd90c2265b4c0243568
ajithkumark16 <ajithpoochur12@gmail.com>
2026-01-05 18:31:54 +0530

    TESRT

--- src/pages/Purchasereturn/Purchasereturn.jsx
+++ src/pages/Purchasereturn/Purchasereturn.jsx
@@ -1646,7 +1646,7 @@ if (e.key === 'Enter') {
           gstType: billDetails.gstType || 'G',
         },
         iledger: {
-          vrNo: voucherNo,
+          vrNo: billDetails.purNo,
           less: parseFloat(addLessAmount) || 0,
           subTotal: totals.subTotal,
           total: totals.total,
