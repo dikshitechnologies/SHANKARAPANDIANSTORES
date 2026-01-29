2473449ea3b8db71c6e422f88ce50f9ed061a01e
md0927 <manisekar0927@gmail.com>
2026-01-29 16:40:22 +0530

    changes

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -2,8 +2,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  // Use http to match the API host used in your environment (dikshiserver)
-  baseURL: "http://dikshiserver/spstorewebapi/api/",
+ 
+  baseURL: "https://dikshi.ddns.net/spstorewebapi/api/",
   timeout: 10000,
   headers: {
     "Content-Type": "application/json",
@@ -15,7 +15,7 @@ export default axiosInstance;
 
 
 export const api = axios.create({
-  baseURL: "http://dikshiserver/spstorewebapi/api/",
+  baseURL: "https://dikshi.ddns.net/spstorewebapi/api/",
   timeout: 10000,
   headers: {
     "Content-Type": "application/json",
