f8ff4cfd927e78ae2dfda5c0abd576d24b978c35
nazeemadikshi-m <nazeemadikshi@gmail.com>
2026-02-02 16:06:00 +0530

    app

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Navbar from './components/Navbar/Navbar'
 import Login from './pages/Login/Login'
 import { useAuth } from './context/AuthContext'
 import TestPage from './pages/TestPage/TestPage'
+import TestPage3 from './pages/TestPage/TestPage3'
 import  Company  from './pages/company/Company'
 import LedgerCreation from './pages/Ledgercreation/Ledgercreation'
 import LedgerGroupCreation from './pages/Ledgergroupcreation/Ledgergroupcreation'
@@ -110,6 +111,7 @@ function App() {
       {/* Protected Routes with Navbar */}
       <Route path="/home" element={<ProtectedRoute><LayoutWithNavbar><Home /></LayoutWithNavbar></ProtectedRoute>} />
       <Route path="/test" element={<ProtectedRoute><LayoutWithNavbar><TestPage /></LayoutWithNavbar></ProtectedRoute>} />
+      <Route path="/test3" element={<ProtectedRoute><LayoutWithNavbar><TestPage3 /></LayoutWithNavbar></ProtectedRoute>} />
           <Route path="/masters/company-creation" element={<ProtectedRoute><LayoutWithNavbar><Company /></LayoutWithNavbar></ProtectedRoute>} />
           <Route path="/sales-invoice" element={<ProtectedRoute><LayoutWithNavbar><SalesInvoice /></LayoutWithNavbar></ProtectedRoute>} />
           <Route path="/masters/ledger-creation" element={<ProtectedRoute><LayoutWithNavbar><LedgerCreation /></LayoutWithNavbar></ProtectedRoute>} />
