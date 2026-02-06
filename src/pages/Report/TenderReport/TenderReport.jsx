9346beb63b3d547798cf38cce392ce024bf4b24b
Aiswarya <aiswaryab1810@gmail.com>
2026-02-06 18:02:06 +0530

    up

--- src/pages/Report/TenderReport/TenderReport.jsx
+++ src/pages/Report/TenderReport/TenderReport.jsx
@@ -185,12 +185,12 @@ const { hasPrintPermission, checkPrintPermission } =
   };
 
   const handleSearch = async (isNewSearch = true) => {
-    if (!fromDate || !toDate || selectedBranches.length === 0) {
-      toast.warning('Please fill all fields: From Date, To Date, and select at least one branch', {
-        autoClose: 2000,
-      });
-      return;
-    }
+    // if (!fromDate || !toDate || selectedBranches.length === 0) {
+    //   toast.warning('Please fill all fields: From Date, To Date, and select at least one branch', {
+    //     autoClose: 2000,
+    //   });
+    //   return;
+    // }
     
     const currentPage = isNewSearch ? 1 : pageNo;
     
@@ -1514,8 +1514,7 @@ const { hasPrintPermission, checkPrintPermission } =
                 ₹{totalUpi.toLocaleString('en-IN', { minimumFractionDigits: 2, maximumFractionDigits: 2 })}
               </span>
             </div>
-          </div>
-          <div style={styles.buttonGroup}>
+               <div style={styles.buttonGroup}>
            <PrintButton 
   onClick={handlePrintClick}
   isActive={hasPrintPermission}
@@ -1529,6 +1528,8 @@ const { hasPrintPermission, checkPrintPermission } =
 />
 
           </div>
+          </div>
+       
         </div>
       )}
 
